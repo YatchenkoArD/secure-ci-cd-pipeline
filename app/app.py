b0d96c0c98c1d25cbf7827e86296f58f6a29a6b9
Third wheel <theyotuberdit@gmail.com>
2026-05-01 22:28:43 +0300

    add sast

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+import subprocess
+from flask import Flask, request
 import os
 
 app = Flask(__name__)
 
-# --- СЕКРЕТЫ ДЛЯ ТЕСТИРОВАНИЯ SECRET DETECTION ---
 AWS_KEY = "AKIAIMNO789ABCDEF001" 
 AWS_SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 GOOGLE_KEY = "AIzaSyA1234567890BCDEF1234567890ABCDEF"
@@ -15,6 +15,12 @@ DATABASE_URL = "postgresql://admin:SuperSecretPassword123@db-prod.finenomore.int
 def index():
     return "<h1>FineNoMore API is running</h1><p>Security scan in progress...</p>"
 
+@app.route('/dns')
+def dns_lookup():
+    hostname = request.args.get('hostname')
+    cmd = "nslookup " + hostname
+    data = subprocess.check_output(cmd, shell=True)
+    return data
+
 if __name__ == '__main__':
-    # Включаем debug для разработки
     app.run(host='0.0.0.0', port=5000)
